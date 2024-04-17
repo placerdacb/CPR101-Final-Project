34ce1790eaebfff5033479d823d6935f231be5d1
Priscilla Lacerda <placerda-cavalcante@myseneca.ca>
2024-04-16 23:08:44 -0400

    V3

--- V3/manipulating.c
+++ V3/manipulating.c
@@ -1,30 +1,57 @@
 // MANIPULATING SOURCE
+
+// Disabling secure warnings
 #define CRT_SECURE_NO_WARNINGS
+
+// Defining buffer size for input
 #define BUFFER_SIZE 80
+
+// Including header file for the manipulating module
 #include "manipulating.h"
 
 // V3
-void manipulating (void) {
+void manipulating(void) {
+    // Print start message
     printf("*** Start of Searching Strings Demo ***\n");
-    char haystack[BUFFER_SIZE]; 
-    char needle [BUFFER_SIZE];
-    char* occurrence = NULL;
     
-do {
-    printf ("Type the string (q - to quit):\n"); 
-    fgets (haystack, BUFFER_SIZE, stdin);
-    haystack[strlen (haystack) - 1] = '\0';
-    if (strcmp (haystack, "q") != 0) {
-    printf ("Type the substring: \n"); 
-    fgets (needle, BUFFER_SIZE, stdin);
-    needle[strlen (needle) - 1] = '\0';
-    occurrence = strstr (haystack, needle);
-    if (occurrence)
-        printf("\'%s\' found at %d position\n", needle, (int)(occurrence - haystack));
-    else
-        printf("Not found\n");
-    }
-} while (strcmp (haystack, "q") != 0);
-printf("*** End of Searching Strings Demo ***\n\n");
-
+    // Declare variables
+    char haystack[BUFFER_SIZE]; // Buffer to store input string
+    char needle[BUFFER_SIZE];   // Buffer to store substring
+    char* occurrence = NULL;    // Pointer to store the occurrence of substring
+    
+    // Start of do-while loop to repeatedly search for substring
+    do {
+        // Prompt for input string
+        printf("Type the string (q - to quit):\n");
+        
+        // Read input string from user
+        fgets(haystack, BUFFER_SIZE, stdin);
+        
+        // Remove newline character from input string
+        haystack[strlen(haystack) - 1] = '\0';
+        
+        // Check if input string is not 'q'
+        if (strcmp(haystack, "q") != 0) {
+            // Prompt for substring
+            printf("Type the substring:\n");
+            
+            // Read substring from user
+            fgets(needle, BUFFER_SIZE, stdin);
+            
+            // Remove newline character from substring
+            needle[strlen(needle) - 1] = '\0';
+            
+            // Search for substring in input string
+            occurrence = strstr(haystack, needle);
+            
+            // Check if substring is found
+            if (occurrence)
+                printf("'%s' found at %d position\n", needle, (int)(occurrence - haystack));
+            else
+                printf("Not found\n");
+        }
+    } while (strcmp(haystack, "q") != 0); // Continue loop until input string is 'q'
+    
+    // Print end message
+    printf("*** End of Searching Strings Demo ***\n\n");
 }
--- V3/manipulating.h
+++ V3/manipulating.h
@@ -1,7 +1,15 @@
 // MANIPULATING MODULE HEADER
+
+// Header guard to prevent multiple inclusion
 #ifndef _MANIPULATING_H_
 #define _MANIPULATING_H_
-#include <stdio.h>
-#include <string.h> 
-void manipulating (void);
+
+// Including necessary header files
+#include <stdio.h>    // For standard input/output functions
+#include <string.h>   // For string manipulation functions
+
+// Function prototype for manipulating function
+void manipulating(void);
+
+// End of header guard
 #endif
